9bd4d25cb7710543b1fbe0386860449c9541c3cd
K.YOSHIZAWA <yszwken45@gmail.com>
2026-01-31 20:22:00 +0900

    Update initialize.py only

--- initialize.py
+++ initialize.py
@@ -135,7 +135,7 @@ def initialize_retriever():
     db = Chroma.from_documents(splitted_docs, embedding=embeddings)
 
     # ベクターストアを検索するRetrieverの作成
-    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 3})
+    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 5})
 
 
 def initialize_session_state():
