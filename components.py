3e0995d1f7fe3ed02883814f3216fed7a08cd797
K.YOSHIZAWA <yszwken45@gmail.com>
2026-02-01 21:17:52 +0900

    Update components logic

--- components.py
+++ components.py
@@ -35,6 +35,30 @@ def display_select_mode():
             label_visibility="collapsed"
         )
 
+def render_sidebar():
+    #  --- サイドバーの実装 ---
+    with st.sidebar:
+        st.header("利用目的")
+        # ラジオボタン
+        display_select_mode()   
+
+    st.markdown("---") # 区切り線
+
+    # 選択肢に応じた説明文の表示
+    # 「社内文書検索」の機能説明
+    st.markdown("**【「社内文書検索」を選択した場合】**")
+    # 「st.info()」を使うと青枠で表示される
+    st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
+    # 「st.code()」を使うとコードブロックの装飾で表示される
+    # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
+    st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
+
+    # 「社内問い合わせ」の機能説明
+    st.markdown("**【「社内問い合わせ」を選択した場合】**")
+    st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
+    st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
+            
+
 
 def display_initial_ai_message():
     """
@@ -44,18 +68,7 @@ def display_initial_ai_message():
         # 「st.success()」とすると緑枠で表示される
         st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
         st.markdown("⚠️ 具体的に入力したほうが期待通りの回答を得やすいです。")
-        # 「社内文書検索」の機能説明
-        st.markdown("**【「社内文書検索」を選択した場合】**")
-        # 「st.info()」を使うと青枠で表示される
-        st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
-        # 「st.code()」を使うとコードブロックの装飾で表示される
-        # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-        st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
-
-        # 「社内問い合わせ」の機能説明
-        st.markdown("**【「社内問い合わせ」を選択した場合】**")
-        st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-        st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
+        
 
 
 def display_conversation_log():
@@ -335,18 +348,8 @@ def display_contact_llm_response(llm_response):
 
     return content
 
-def render_sidebar():
-    #  --- サイドバーの実装 ---
-    with st.sidebar:
-        st.header("利用目的")
-        # ラジオボタン
-        display_select_mode()   
 
-    st.markdown("---") # 区切り線
-
-    # 選択肢に応じた説明文の表示
-    display_initial_ai_message()
-    return st.session_state.mode
+    
 
 
 
