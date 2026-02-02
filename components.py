bcceb90ea49d71ca9bd962ef6c05ef138da87b9a
K.YOSHIZAWA <yszwken45@gmail.com>
2026-02-02 21:37:28 +0900

    Update components logic

--- components.py
+++ components.py
@@ -21,19 +21,7 @@ def display_app_title():
     st.markdown(f"## {ct.APP_NAME}")
 
 
-# def display_select_mode():
-#     """
-#     回答モードのラジオボタンを表示
-#     """
-#     # 回答モードを選択する用のラジオボタンを表示
-#     col1, col2 = st.columns([100, 1])
-#     with col1:
-#         # 「label_visibility="collapsed"」とすることで、ラジオボタンを非表示にする
-#         st.session_state.mode = st.radio(
-#             label="",
-#             options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
-#             label_visibility="collapsed"
-#         )
+
 def display_select_mode():
     """
     回答モードのラジオボタンを表示
@@ -49,28 +37,7 @@ def display_select_mode():
     return mode
   
 
-# def render_sidebar():
-#     #  --- サイドバーの実装 ---
-#     with st.sidebar:
-#         st.header("利用目的")
-#         # ラジオボタン
-#         display_select_mode()   
 
-#     st.markdown("---") # 区切り線
-
-#     # 選択肢に応じた説明文の表示
-#     # 「社内文書検索」の機能説明
-#     st.markdown("**【「社内文書検索」を選択した場合】**")
-#     # 「st.info()」を使うと青枠で表示される
-#     st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
-#     # 「st.code()」を使うとコードブロックの装飾で表示される
-#     # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-#     st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
-
-#     # 「社内問い合わせ」の機能説明
-#     st.markdown("**【「社内問い合わせ」を選択した場合】**")
-#     st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-#     st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
 
 def render_sidebar():
     with st.sidebar:
@@ -147,7 +114,7 @@ def display_conversation_log():
                         icon = utils.get_source_icon(message['content']['main_file_path'])
                         # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
                         if "main_page_number" in message["content"]:
-                            st.success(f"{message['content']['main_file_path']}", icon=icon)
+                            st.success(f"{message['content']['main_file_path']} (P.{message['content']['main_page_number']})", icon=icon)
                         else:
                             st.success(f"{message['content']['main_file_path']}", icon=icon)
                         
@@ -164,7 +131,7 @@ def display_conversation_log():
                                 icon = utils.get_source_icon(sub_choice['source'])
                                 # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
                                 if "page_number" in sub_choice:
-                                    st.info(f"{sub_choice['source']}", icon=icon)
+                                    st.info(f"{sub_choice['source']} (P.{sub_choice['page_number']})", icon=icon)
                                 else:
                                     st.info(f"{sub_choice['source']}", icon=icon)
                     # ファイルのありかの情報が取得できなかった場合、LLMからの回答のみ表示
@@ -219,7 +186,7 @@ def display_search_llm_response(llm_response):
             # ページ番号を取得
             main_page_number = llm_response["context"][0].metadata["page"]
             # 「メインドキュメントのファイルパス」と「ページ番号」を表示
-            st.success(f"{main_file_path}", icon=icon)
+            st.success(f"{main_file_path} (P.{main_page_number})", icon=icon)
         else:
             # 「メインドキュメントのファイルパス」を表示
             st.success(f"{main_file_path}", icon=icon)
@@ -254,7 +221,7 @@ def display_search_llm_response(llm_response):
                 # ページ番号を取得
                 sub_page_number = document.metadata["page"]
                 # 「サブドキュメントのファイルパス」と「ページ番号」の辞書を作成
-                sub_choice = {"source": sub_file_path, "page_number": sub_page_number}
+                sub_choice ={"source": f"{sub_file_path} (P.{sub_page_number})"}
             else:
                 # 「サブドキュメントのファイルパス」の辞書を作成
                 sub_choice = {"source": sub_file_path}
@@ -355,7 +322,7 @@ def display_contact_llm_response(llm_response):
                 # ページ番号を取得
                 page_number = document.metadata["page"]
                 # 「ファイルパス」と「ページ番号」
-                file_info = f"{file_path}"
+                file_info = f"{file_path} (P.{page_number})"
             else:
                 # 「ファイルパス」のみ
                 file_info = f"{file_path}"
